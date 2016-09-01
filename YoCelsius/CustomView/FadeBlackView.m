e5e84c2e1333b0ae10ea966447d27842c527271f
zhengmingjun <keshiim@163.com>
2016-09-01 18:29:05 +0800

    添加我媳妇

--- YoCelsius/CustomView/FadeBlackView.m
+++ YoCelsius/CustomView/FadeBlackView.m
@@ -19,9 +19,24 @@
         
         self.backgroundColor = [UIColor blackColor];
         self.alpha           = 0.f;
+        [self addBackgroundLayer];
     }
     
     return self;
+}
+
+- (void)addBackgroundLayer {
+    //设置背景(注意这个图片其实在根图层)
+    UIImage *backgroundImage=[UIImage imageNamed:@"4"];
+//    self.view.backgroundColor=[UIColor colorWithPatternImage:backgroundImage];
+    
+    //自定义一个图层
+    CALayer *_layer=[[CALayer alloc]init];
+    _layer.backgroundColor = [UIColor clearColor].CGColor;
+    _layer.bounds          = CGRectMake(0, 0, backgroundImage.size.width, backgroundImage.size.height);
+    _layer.contents        = (id)backgroundImage.CGImage;
+    _layer.position        = self.center;
+    [self.layer addSublayer:_layer];
 }
 
 - (void)show {
